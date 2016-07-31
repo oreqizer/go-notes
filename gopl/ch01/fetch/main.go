d21d881765633c378c597360d6ece95ba42d942f
oreqizer <oreqizer@gmail.com>
2016-07-31 18:44:16 +0200

    ex 1.7, 1.8, 1.9

--- gopl/ch01/fetch/main.go
+++ gopl/ch01/fetch/main.go
@@ -9,25 +9,30 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
+	"io"
+	"strings"
 )
 
 func main() {
-	for _, url := range os.Args[1:] {
+	for _, rawurl := range os.Args[1:] {
+		url := rawurl
+		prefix := "http://"
+		if !strings.HasPrefix(url, prefix) {
+			url = prefix + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		_, err = io.Copy(os.Stdout, resp.Body)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			fmt.Fprintf(os.Stderr, "fetch: copy: %v\n", err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("Status code: %s\n", resp.Status)
 	}
 }
 
